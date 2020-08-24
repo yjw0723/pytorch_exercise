a854b658b7524edaf033c030f3b4c06f1caa5126
yjw8860 <botoks@naver.com>
2020-08-24 12:17:33 +0900

    Image classification 코드 수정

--- classification_tutorial.py
+++ classification_tutorial.py
@@ -1,72 +1,7 @@
 # -*- coding: utf-8 -*-
-"""
-분류기(Classifier) 학습하기
-============================
-
-지금까지 어떻게 신경망을 정의하고, 손실을 계산하며 또 가중치를 갱신하는지에
-대해서 배웠습니다.
-
-이제 아마도 이런 생각을 하고 계실텐데요,
-
-데이터는 어떻게 하나요?
-------------------------
-
-일반적으로 이미지나 텍스트, 오디오나 비디오 데이터를 다룰 때는 표준 Python 패키지를
-이용하여 NumPy 배열로 불러오면 됩니다. 그 후 그 배열을 ``torch.*Tensor`` 로 변환합니다.
-
--  이미지는 Pillow나 OpenCV 같은 패키지가 유용합니다.
--  오디오를 처리할 때는 SciPy와 LibROSA가 유용하고요.
--  텍스트의 경우에는 그냥 Python이나 Cython을 사용해도 되고, NLTK나 SpaCy도
-   유용합니다.
-
-특별히 영상 분야를 위한 ``torchvision`` 이라는 패키지가 만들어져 있는데,
-여기에는 Imagenet이나 CIFAR10, MNIST 등과 같이 일반적으로 사용하는 데이터셋을 위한
-데이터 로더(data loader), 즉 ``torchvision.datasets`` 과 이미지용 데이터 변환기
-(data transformer), 즉 ``torch.utils.data.DataLoader`` 가 포함되어 있습니다.
-
-이러한 기능은 엄청나게 편리하며, 매번 유사한 코드(boilerplate code)를 반복해서
-작성하는 것을 피할 수 있습니다.
-
-이 튜토리얼에서는 CIFAR10 데이터셋을 사용합니다. 여기에는 다음과 같은 분류들이
-있습니다: '비행기(airplane)', '자동차(automobile)', '새(bird)', '고양이(cat)',
-'사슴(deer)', '개(dog)', '개구리(frog)', '말(horse)', '배(ship)', '트럭(truck)'.
-그리고 CIFAR10에 포함된 이미지의 크기는 3x32x32로, 이는 32x32 픽셀 크기의 이미지가
-3개 채널(channel)의 색상로 이뤄져 있다는 것을 뜻합니다.
-
-.. figure:: /_static/img/cifar10.png
-   :alt: cifar10
-
-   cifar10
-
-
-이미지 분류기 학습하기
-----------------------------
-
-다음과 같은 단계로 진행해보겠습니다:
-
-1. ``torchvision`` 을 사용하여 CIFAR10의 학습용 / 시험용 데이터셋을
-   불러오고, 정규화(nomarlizing)합니다.
-2. 합성곱 신경망(Convolution Neural Network)을 정의합니다.
-3. 손실 함수를 정의합니다.
-4. 학습용 데이터를 사용하여 신경망을 학습합니다.
-5. 시험용 데이터를 사용하여 신경망을 검사합니다.
-
-1. CIFAR10를 불러오고 정규화하기
-^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
-
-``torchvision`` 을 사용하여 매우 쉽게 CIFAR10을 불러올 수 있습니다.
-"""
 import torch
 import torchvision
 import torchvision.transforms as transforms
-
-########################################################################
-# torchvision 데이터셋의 출력(output)은 [0, 1] 범위를 갖는 PILImage 이미지입니다.
-# 이를 [-1, 1]의 범위로 정규화된 Tensor로 변환합니다.
-#
-# .. note::
-#     만약 Windows 환경에서 BrokenPipeError가 발생한다면,
-#     torch.utils.data.DataLoader()의 num_worker를 0으로 설정해보세요.
 
 transform = transforms.Compose(
     [transforms.ToTensor(),
@@ -84,12 +19,6 @@ testloader = torch.utils.data.DataLoader(testset, batch_size=64,
 
 classes = ('plane', 'car', 'bird', 'cat',
            'deer', 'dog', 'frog', 'horse', 'ship', 'truck')
-
-########################################################################
-# 2. 합성곱 신경망(Convolution Neural Network) 정의하기
-# ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
-# 이전의 신경망 섹션에서 신경망을 복사한 후, (기존에 1채널 이미지만 처리하도록
-# 정의된 것을) 3채널 이미지를 처리할 수 있도록 수정합니다.
 
 import torch.nn as nn
 import torch.nn.functional as F
@@ -117,24 +46,10 @@ class Net(nn.Module):
 
 net = Net()
 
-########################################################################
-# 3. 손실 함수와 Optimizer 정의하기
-# ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
-# 교차 엔트로피 손실(Cross-Entropy loss)과 모멘텀(momentum) 값을 갖는 SGD를 사용합니다.
-
 import torch.optim as optim
 
 criterion = nn.CrossEntropyLoss()
 optimizer = optim.SGD(net.parameters(), lr=0.001, momentum=0.9)
-
-########################################################################
-# 4. 신경망 학습하기
-# ^^^^^^^^^^^^^^^^^^^^
-#
-# 이제 재미있는 부분이 시작됩니다.
-# 단순히 데이터를 반복해서 신경망에 입력으로 제공하고, 최적화(Optimize)만 하면
-# 됩니다.
-
 
 for epoch in range(2):   # 데이터셋을 수차례 반복합니다.
     running_loss = 0.0
@@ -156,9 +71,6 @@ for epoch in range(2):   # 데이터셋을 수차례 반복합니다.
         if i % 100 == 99:    # print every 100 mini-batches
             running_loss = 0.0
 print('Finished Training')
-#
-# ########################################################################
-# # 학습한 모델을 저장해보겠습니다:
-#
-# PATH = './cifar_net.pth'
-# torch.save(net.state_dict(), PATH)
+
+PATH = './cifar_net.pth'
+torch.save(net.state_dict(), PATH)
--- image_classification_example.py
+++ image_classification_example.py
@@ -1,11 +1,13 @@
 import torch
-import torchvision
 from torchvision import datasets, transforms, models
-import torch.nn as nn
 import torch.nn.functional as F
+import torch.nn as nn
 import torch.optim as optim
 from torch.utils.data.sampler import SubsetRandomSampler
 import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+from tqdm import tqdm
 
 train_dir = './redness/train'
 test_dir = './redness/test'
@@ -26,26 +28,34 @@ trainloader = torch.utils.data.DataLoader(train_data,
 testloader = torch.utils.data.DataLoader(test_data,
                                          sampler=test_sampler, batch_size=1)
 
-model = models.resnet50(pretrained=True)
-for param in model.parameters():
+net = models.resnet50(pretrained=True)
+for param in net.parameters():
     param.requires_grad = False
 
-model.fc = nn.Sequential(nn.Linear(2048, 512),
-                         nn.ReLU(),
-                         nn.Dropout(0.2),
-                         nn.Linear(512, 4))
+net.fc = nn.Sequential(nn.Linear(2048, 512),
+                       nn.ReLU(),
+                       nn.Dropout(0.2),
+                       nn.Linear(512, 4),
+                       nn.Softmax())
 
 criterion = nn.CrossEntropyLoss()
-optimizer = optim.Adam(model.fc.parameters(), lr=0.003)
-model.to(device)
+optimizer = optim.Adam(net.parameters(), lr=0.01)
+net.to(device)
 
 TOTAL_EPOCH = 10
 train_avg_loss = []
 test_avg_loss = []
+train_accuracy_list = []
+test_accuracy_list = []
+
 for epoch in range(10):   # 데이터셋을 수차례 반복합니다.
     train_loss_list = []
     test_loss_list = []
-    for i, data in enumerate(trainloader, 0):
+
+    total = 0
+    correct = 0
+    print(f'START TRAINING EPOCH:{epoch}')
+    for i, data in enumerate(tqdm(trainloader), 0):
         # [inputs, labels]의 목록인 data로부터 입력을 받은 후;
         inputs, labels = data
         inputs, labels = inputs.to(device), labels.to(device)
@@ -54,22 +64,66 @@ for epoch in range(10):   # 데이터셋을 수차례 반복합니다.
         optimizer.zero_grad()
 
         # 순전파 + 역전파 + 최적화를 한 후
-        outputs = model.forward(inputs)
+        outputs = net.forward(inputs)
+
+        _, predicted = torch.max(outputs.data, 1)
+        total += labels.size(0)
+        correct += (predicted == labels).sum().item()
+
         loss = criterion(outputs, labels)
-        print('outputs:', outputs.type(), outputs)
-        print('labels:',labels.type(), labels)
         loss.backward()
         optimizer.step()
 
         # 통계를 출력합니다.
         train_loss_list.append(loss.item())
-        print(f'iteration:{i}/{len(trainloader)}|loss:{round(loss.item(),3)}')
+    train_accuracy = 100*correct/total
+    train_accuracy_list.append(train_accuracy)
+    train_avg_loss.append(round(np.mean(train_loss_list), 3))
 
-    for inputs, labels in testloader:
-        inputs, labels = inputs.to(device), labels.to(device)
-        logps = model.forward(inputs)
-        test_loss_list.append(criterion(logps, labels).item())
-    print(f'EPOCH:{epoch}/{TOTAL_EPOCH}|TRAIN LOSS(AVG):{round(np.mean(train_loss_list),3)}|VALIDATION LOSS(AVG):{round(np.mean(test_loss_list),3)}')
+    print(f'START VALIDATION EPOCH:{epoch}')
+    correct = 0
+    total = 0
+    with torch.no_grad():
+        for inputs, labels in tqdm(testloader):
+            inputs, labels = inputs.to(device), labels.to(device)
+            outputs =net(inputs)
+            _, predicted = torch.max(outputs.data, 1)
+            total += labels.size(0)
+            correct += (predicted == labels).sum().item()
+            test_loss_list.append(criterion(outputs, labels).item())
+        test_accuracy = 100 * correct / total
+        test_accuracy_list.append(test_accuracy)
+        test_avg_loss.append(round(np.mean(test_loss_list), 3))
+    print(f'EPOCH:{epoch}/{TOTAL_EPOCH}'
+          f'|TRAIN LOSS(AVG):{round(np.mean(train_loss_list),3)}'
+          f'|TRAIN ACCURACY:{train_accuracy}'
+          f'|VALIDATION LOSS(AVG):{round(np.mean(test_loss_list),3)}'
+          f'|VALIDATION ACCURACY:{test_accuracy}')
 
 print('Finished Training')
-torch.save(model, 'redness.pth')
+torch.save(net, 'redness.pth')
+
+df = pd.DataFrame({'epoch': list(range(TOTAL_EPOCH)),
+                   'train_loss': train_avg_loss,
+                   'train_accuracy': train_accuracy_list,
+                   'test_loss':test_avg_loss,
+                   'test_accuracy':test_accuracy_list},
+                  columns=['epoch', 'train_loss', 'train_accuracy', 'test_loss', 'test_accuracy'])
+df_save_path = './redness_loss.csv'
+df.to_csv(df_save_path, index=False, encoding='euc-kr')
+
+plt.plot(list(range(TOTAL_EPOCH)), train_avg_loss, 'b', label='Training Loss')
+plt.plot(list(range(TOTAL_EPOCH)), test_avg_loss, 'r', label='Validation Loss')
+plt.title('Training and validation loss')
+plt.legend()
+save_path = 'redness_loss.png'
+plt.savefig(save_path)
+plt.cla()
+
+plt.plot(list(range(TOTAL_EPOCH)), train_accuracy_list, 'b', label='Training Accuracy')
+plt.plot(list(range(TOTAL_EPOCH)), test_accuracy_list, 'r', label='Validation Accuracy')
+plt.title('Training and validation Accuracy')
+plt.legend()
+save_path = 'redness_accuracy.png'
+plt.savefig(save_path)
+plt.cla()
